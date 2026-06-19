2327ea2d118991016c72c6d0850499d952d05316
duongKoNgu <115865433+duongKoNgu@users.noreply.github.com>
2026-06-19 14:26:50 +0700

    .

--- movie-system/movie-service/src/main/java/com/movie/MovieServiceApplication.java
+++ movie-system/movie-service/src/main/java/com/movie/MovieServiceApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Gateway Server")})
 @SpringBootApplication
+@EnableScheduling
 public class MovieServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieServiceApplication.class, args);
